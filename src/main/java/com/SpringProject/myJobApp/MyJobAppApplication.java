1a5a99eb7bea8133f26782fc76f66cb3b1482716
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-08-15 23:07:50 +0530

    hello

---
+++ src/main/java/com/SpringProject/myJobApp/MyJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringProject.myJobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJobAppApplication.class, args);
+	}
+
+}
